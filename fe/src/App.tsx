52324f778999a7c26bf15fd1cd4d50c192f8d776
Frederico Nogueira <fredericonogueira01@outlook.com>
2024-12-31 10:32:58 -0300

    feat: Configs

---
+++ fe/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+import { GlobalStyles } from "./styles/GlobalStyles"
+
+export function App() {
+  return (
+    <React.Fragment>
+      <GlobalStyles />
+      <h1>APP</h1>
+    </React.Fragment>
+  )
+}
